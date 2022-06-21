70c35744d5a106bae075298e8d900c7c8d44d4ae
m16khb <habin@gimhabin-ui-MacBookAir.local>
2022-06-21 17:35:25 +0900

    @Query , @Param, @Post Decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Request } from 'express';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,32 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getIndex(@Req() req: Request): string {
+    console.log(req);
     return this.appService.getHello();
+  }
+
+  //쿼리스트링
+  @Get('/hello')
+  getHello(@Query() query: any): string {
+    return query;
+  }
+
+  //패스스트링
+  @Get('/bye/:id')
+  getBye(@Param('id') userId: string) {
+    return userId;
+  }
+
+  //패스스트링
+  @Get('/byebye/:id/:password')
+  getByeBye(@Param() params: { [key: string]: string }) {
+    return params;
+  }
+
+  //Body
+  @Post('/test')
+  getTest(@Body() body: any): string {
+    return body.a;
   }
 }
